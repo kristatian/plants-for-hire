7e9a171fba42f769ba0348e4a26e9dd2f2fdc77f
kristatian <kristatian@gmail.com>
2025-01-15 21:51:24 -0500

    added togglable dark and light modes

--- logic.js
+++ logic.js
@@ -1,4 +1,28 @@
 
+function toggleDarkMode() {
+    const currentMode = document.getElementById("dark-mode-btn-img").getAttribute("alt");
+    console.log(currentMode);
+    currentMode === "wakege" ? setDarkMode() : setLightMode();
+}
+
+function setDarkMode() {
+    const img = document.getElementById("dark-mode-btn-img");
+    img.src = "./assets/bedge.png";
+    img.alt = "bedge";
+    document.body.style.backgroundColor = "#2e3440";
+    document.getElementById("dark-mode-btn").style.backgroundColor = "#2e3440";
+    document.getElementById("text").style.color = "#eceff4";
+}
+
+function setLightMode() {
+    const img = document.getElementById("dark-mode-btn-img");
+    img.src = "./assets/wakege.png";
+    img.alt = "wakege";
+    document.body.style.backgroundColor = "#eceff4";
+    document.getElementById("dark-mode-btn").style.backgroundColor = "#eceff4";
+    document.getElementById("text").style.color = "#2e3440";
+}
+
 function getAdj() {
     fetch('lovely.json').then(response => {
         return response.json();
